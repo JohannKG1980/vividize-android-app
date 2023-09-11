442f60c2fa2388d212ead9caaa1e813f4f0cf994
Johann Kucharczyk-Gentsch <johann.kg@icloud.com>
2023-09-11 16:05:53 +0200

    First Layout experiments

--- app/src/main/java/com/example/vividize_unleashyourself/MainActivity.kt
+++ app/src/main/java/com/example/vividize_unleashyourself/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.vividize_unleashyourself
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vividize_unleashyourself.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.background = null
     }
 }
