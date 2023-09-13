23a4f42fdce7620cd4c8464c623f7edd63ab0e7f
Johann Kucharczyk-Gentsch <johann.kg@icloud.com>
2023-09-13 07:38:52 +0200

    Motion Layout and motion in home Fragment implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,23 +34,23 @@ android {
          viewBinding = true
      }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/vividize_unleashyourself/ui/HomeFragment.kt
+++ app/src/main/java/com/example/vividize_unleashyourself/ui/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +17,7 @@ import com.example.vividize_unleashyourself.databinding.FragmentHomeBinding
 import eightbitlab.com.blurview.BlurView
 import eightbitlab.com.blurview.RenderEffectBlur
 import eightbitlab.com.blurview.RenderScriptBlur
+
 
 class HomeFragment : Fragment() {
 
@@ -30,7 +33,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         binding = FragmentHomeBinding.inflate(layoutInflater)
+
         return binding.root
     }
 
@@ -38,12 +43,43 @@ class HomeFragment : Fragment() {
     // @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-         //  binding.blurView1.setRenderEffect(RenderEffect.createBlurEffect(10F, 10F, Shader.TileMode.DECAL))
-
+        //  binding.blurView1.setRenderEffect(RenderEffect.createBlurEffect(10F, 10F, Shader.TileMode.DECAL))
 
 
         binding.blurView1.setupWith(binding.root, RenderScriptBlur(requireContext()))
             .setFrameClearDrawable(binding.ivHomeBg.drawable) // Optional
             .setBlurRadius(3f)
+
+
+        view.post {
+            val quote =
+                "Du musst es dir in deinem Herzen und deinem Verstand vorstellen, bevor du es empfangen kannst. Wenn du glaubst, dann sind alle Dinge möglich."
+
+            val stringBuilder = StringBuilder()
+            Thread {
+                for (letter in quote) {
+                    stringBuilder.append(letter)
+                    Thread.sleep(10)
+
+                    activity?.runOnUiThread {
+                        binding.tvQuote.text = stringBuilder.toString()
+                    }
+                }
+            }.start()
+        }
+
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val motionLayout = binding.constraintLayout2
+            motionLayout.transitionToEnd()
+        }, 2000)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val motionLayout = binding.constraintLayout4
+            motionLayout.transitionToEnd()
+        }, 3000)
+
     }
 }
