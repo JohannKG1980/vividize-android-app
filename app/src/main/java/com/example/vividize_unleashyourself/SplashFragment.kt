acce249294b1bf2d2c38c6f5be4b33712c89801d
Johann Kucharczyk-Gentsch <johann.kg@icloud.com>
2023-09-15 17:56:47 +0200

    splash screen finished

--- app/src/main/java/com/example/vividize_unleashyourself/SplashFragment.kt
+++ app/src/main/java/com/example/vividize_unleashyourself/SplashFragment.kt
@@ -7,12 +7,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.vividize_unleashyourself.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-private lateinit var binding: FragmentSplashBinding
+    private lateinit var binding: FragmentSplashBinding
+    private val viewModel: MainViewModel by activityViewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,9 +32,46 @@ private lateinit var binding: FragmentSplashBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        binding.clSplashBg.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+                viewModel.todaysQuote.observe(viewLifecycleOwner) {}
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                // Hier können Sie Code hinzufügen, der während der Transition ausgeführt werden soll
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                // Überprüfen Sie, ob die aktuelle ID der Endzustand des äußeren MotionLayouts ist
+                if (currentId == R.id.totalEnd) {
+                    // Starten Sie die innere Animation
+                    binding.clAppName.transitionToEnd()
+                }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                // Hier können Sie Code hinzufügen, der ausgeführt werden soll, wenn ein Trigger während der Transition ausgelöst wird
+            }
+        })
         Handler(Looper.getMainLooper()).postDelayed({
             findNavController().navigate(R.id.action_splashScreenFragment_to_fullscreenFragment)
-        }, 3000)
+        }, 6000)
     }
 
 }
