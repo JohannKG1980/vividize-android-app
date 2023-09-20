89ccaca39851cb9afd9dc117c62e1e82aa38650c
Johann Kucharczyk-Gentsch <johann.kg@icloud.com>
2023-09-20 17:54:09 +0200

    implenented a custom animated extension dialog on clicking the fab

--- app/src/main/java/com/example/vividize_unleashyourself/MainActivity.kt
+++ app/src/main/java/com/example/vividize_unleashyourself/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.vividize_unleashyourself
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.activity.viewModels
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -23,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.blurView1.setupWith(binding.root, RenderScriptBlur(this))
             .setBlurRadius(3f)
+        binding.blurViewAddDialog.setupWith(binding.root, RenderScriptBlur(this))
+            .setBlurRadius(3f)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         navController = navHostFragment.navController
@@ -32,6 +39,54 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.bottomNavigationView
 
         navView.setupWithNavController(navController)
+
+        val motionLayout = binding.clBottomDialog
+        binding.fab.setOnClickListener {
+
+            if (motionLayout.currentState == R.id.start) {
+                binding.clBottomDialog.visibility = VISIBLE
+                motionLayout.transitionToEnd()
+            } else {
+                motionLayout.transitionToStart()
+
+                binding.clBottomDialog.setTransitionListener(object : MotionLayout.TransitionListener {
+                    override fun onTransitionStarted(
+                        motionLayout: MotionLayout?,
+                        startId: Int,
+                        endId: Int
+                    ) {
+
+                    }
+
+                    override fun onTransitionChange(
+                        motionLayout: MotionLayout?,
+                        startId: Int,
+                        endId: Int,
+                        progress: Float
+                    ) {
+
+                    }
+
+
+                    override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                        // Überprüfen Sie, ob die aktuelle ID der Endzustand des äußeren MotionLayouts ist
+                        if (currentId == R.id.start) {
+                            binding.clBottomDialog.visibility = GONE
+                        }
+                    }
+
+                    override fun onTransitionTrigger(
+                        motionLayout: MotionLayout?,
+                        triggerId: Int,
+                        positive: Boolean,
+                        progress: Float
+                    ) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            }
+        }
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
@@ -66,7 +121,6 @@ class MainActivity : AppCompatActivity() {
 
                 }
             }
-
         }
     }
 
