99aeda2a1b72ff08a6cd663a9ea30212a52d8ee3
Johann Kucharczyk-Gentsch <johann.kg@icloud.com>
2023-09-21 01:40:30 +0200

    Implementing Sedona Method feature in progress

--- app/src/main/java/com/example/vividize_unleashyourself/data/model/FiveSteps.kt
+++ app/src/main/java/com/example/vividize_unleashyourself/data/model/FiveSteps.kt
@@ -1,10 +1,43 @@
 package com.example.vividize_unleashyourself.data.model
 
-data class FiveSteps(
-    var stepOne: String,
-    var stepTwo: Boolean,
-    var stepThree: Boolean,
-    var stepFour: String,
-    var checkIntensity: Int,
-    var stepFive: Boolean
-)
+import com.example.vividize_unleashyourself.R
+
+data class FiveSteps(val cycleId: Int) {
+    val stepOne = Question(R.string.stepOne)
+    val stepOneRepeat = Question(R.string.stepOneRepeat)
+    val stepOneInput = ""
+
+
+    val stepTwoInstruct = Question(R.string.stepTwoGeneral)
+    val stepTwo = listOf(
+        Question(R.string.stepTwo1),
+        Question(R.string.stepTwo2),
+        Question(R.string.stepTwo3)
+    )
+    val stepTwoQuestion = stepTwo.random()
+    var stepTwoAnswer = false
+
+
+    val stepThree = listOf(
+        Question(R.string.stepThree1),
+        Question(R.string.stepThree2)
+    )
+    val stepThreeQuestion = stepThree.random()
+    var stepThreeAnswer = false
+
+    val stepThreeFollowUp = Question(R.string.stepThreeNo)
+    var stepThreeFollowAnswer = false
+
+
+    val stepFour = Question(R.string.stepFour)
+    var stepFourAnswer = ""
+
+
+    val stepFive = Question(R.string.stepFive)
+    val intensity = 0
+
+
+    val repeatQuestion = Question(R.string.askForRepeat)
+    var repeatAnswer = false
+
+}
---
+++ app/src/main/java/com/example/vividize_unleashyourself/data/model/Question.kt
@@ -0,0 +1,3 @@
+package com.example.vividize_unleashyourself.data.model
+
+data class Question(val content: Int, val audioPath: String = "")
